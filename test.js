b3e77cd278d3f65ae71c725a188fb4b1299ee65e
Knut Kirkhorn <knutkirk@hotmail.com>
2019-07-02 23:59:53 +0200

    Use `nock` to mock http response

--- test.js
+++ test.js
@@ -1,7 +1,59 @@
 import test from 'ava';
+import nock from 'nock';
 import m from '.';
 
+const exampleCommit = {
+    id: '9927771437',
+    type: 'PushEvent',
+    actor: {
+        id: 14054353,
+        login: 'Knutakir',
+        display_login: 'Knutakir',
+        gravatar_id: '',
+        url: 'https://api.github.com/users/Knutakir',
+        avatar_url: 'https://avatars.githubusercontent.com/u/14054353?'
+    },
+    repo: {
+        id: 194443339,
+        name: 'Knutakir/gh-latest-commit',
+        url: 'https://api.github.com/repos/Knutakir/gh-latest-commit'
+    },
+    payload: {
+        push_id: 3771517766,
+        size: 1,
+        distinct_size: 1,
+        ref: 'refs/heads/master',
+        head: 'b0e6e77d81766eb680b9bbeff47a6db932d24811',
+        before: 'f1e8c8f06c75dc74871ed47f4ae8126990680e45',
+        commits: [
+            {
+                sha: 'b0e6e77d81766eb680b9bbeff47a6db932d24811',
+                author: {
+                    email: 'knutkirk@hotmail.com',
+                    name: 'Knut Kirkhorn'
+                },
+                message: 'Fix typo and check error type',
+                distinct: true,
+                url: 'https://api.github.com/repos/Knutakir/gh-latest-commit/commits/b0e6e77d81766eb680b9bbeff47a6db932d24811'
+            }
+        ]
+    },
+    public: true,
+    created_at: '2019-07-01T17:39:38Z'
+};
+
+test.after(() => {
+    // Clean all nocks
+    nock.cleanAll();
+});
+
 test('returns latest commit', async t => {
+    nock('https://api.github.com')
+        .get('/users/knutakir/events/public')
+        .reply(200, [
+            exampleCommit
+        ]);
+        
     try {
         const commit = await m('knutakir');
         t.is(typeof commit, 'object');
@@ -11,6 +63,12 @@ test('returns latest commit', async t => {
 });
 
 test('returns latest commit with given email', async t => {
+    nock('https://api.github.com')
+        .get('/users/knutakir/events/public')
+        .reply(200, [
+            exampleCommit
+        ]);
+
     try {
         const commit = await m('knutakir', 'knutkirk@hotmail.com');
         t.is(typeof commit, 'object');
@@ -30,7 +88,7 @@ test('throws when provided a non string email', async t => {
     }
 });
 
-test('throws when provided a invalid email address', async t => {
+test('throws when provided an invalid email address', async t => {
     const expectedValue = '`email` should be an email address';
 
     try {
